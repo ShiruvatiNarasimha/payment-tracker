037c0d7e47c067b38ff2d69bebdf4ca90cabcd0f
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-09-26 22:20:18 +0530

    Zod

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,45 +1,43 @@
-import HeaderBox from '@/components/HeaderBox'
-import RightSidebar from '@/components/RightSidebar';
-import TotalBalanceBox from '@/components/TotalBalanceBox';
-import React from 'react';
-
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
-
-    const loggedIn = {firstName: "Shiruvati", lastName: 'Narasimha', email:"mrnattyboy05@gamil.com" }
-   
+  const loggedIn = {
+    firstName: "Shiruvati",
+    lastName: "Narasimha",
+    email: "mrnattyboy05@gamil.com",
+  };
 
   return (
-    <section className='home'>
-        <div className='home-content'>
-            <header className='home-header'>
-                <HeaderBox
-
-                type="greeting"
-                title="welcome"
-                user={loggedIn?.firstName || 'Guest'}
-                subtext="Access and manage 
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage 
                 your account and 
                 transactions efficiently."
-                />
+          />
 
-                <TotalBalanceBox
-                accounts={[]}
-                totalBanks={1}
-                totalCurrentBalance={1250.35}
-                >
-                </TotalBalanceBox>
-            </header>
-
-            RECENT TRANSACTIONS
-        </div>
-        <RightSidebar
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          ></TotalBalanceBox>
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
         user={loggedIn}
         transactions={[]}
-        banks={[{currentBalance: 123.50}, {currentBalance: 2000.90}]}
-        />
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 2000.9 }]}
+      />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -17,25 +17,22 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
+import CustomInput from "./CustomInput";
+import { authFormSchema } from "@/lib/utils";
 
 const AuthForm = ({ type }: { type: string }) => {
   const [user, setUser] = useState(null);
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof authFormSchema>>({
+    resolver: zodResolver(authFormSchema),
     defaultValues: {
-      username: "",
+      email: "",
+      password: "0",
     },
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof authFormSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values);
@@ -74,7 +71,7 @@ const AuthForm = ({ type }: { type: string }) => {
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
               <FormField
                 control={form.control}
-                name="username"
+                name="email"
                 render={({ field }) => (
                   <div className="form-item">
                     <FormLabel className="form-label">Email</FormLabel>
@@ -86,6 +83,40 @@ const AuthForm = ({ type }: { type: string }) => {
                           {...field}
                         />
                       </FormControl>
+                      <FormMessage className="form-message mt-2" />
+                    </div>
+                  </div>
+                )}
+              />
+              <CustomInput
+                control={form.control}
+                name="username"
+                label="username"
+                placeholder="Enter your username"
+              />
+              <CustomInput
+                control={form.control}
+                name="username"
+                label="username"
+                placeholder="Enter your username"
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <div className="form-item">
+                    <FormLabel className="form-label">Password</FormLabel>
+                    <div className="flex w-full flex-col">
+                      <FormControl>
+                        <Input
+                          placeholder="Enter your password"
+                          className="input-class"
+                          type="password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage className="form-message mt-2" />
                     </div>
                   </div>
                 )}
